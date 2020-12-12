d9b780b80aef83f2b8f179c199a8ea40b14c167f
levangagnidze <levan@toptal.com>
2020-12-12 18:27:06 +0400

    Initial Commit

---
+++ Healthy Food/Healthy_FoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  Healthy_FoodApp.swift
+//  Healthy Food
+//
+//  Created by Leo on 12.12.20.
+//
+
+import SwiftUI
+
+@main
+struct Healthy_FoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
